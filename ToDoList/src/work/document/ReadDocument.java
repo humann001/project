7dbcacf224d1ea53cf713340f54f450ee42d74de
виталик рыбаков <vita250r@yandex.ru>
2019-11-10 23:09:43 +0300

    test2

---
+++ ToDoList/src/work/document/ReadDocument.java
@@ -0,0 +1,103 @@
+package work.document;
+
+import java.util.*;
+
+public class ReadDocument {
+
+    private Storage storage;
+
+    public ReadDocument(String nameFile) {
+        storage = new Storage(nameFile);
+    }
+
+    public void inputTasks(String nameParametr, String value) {
+        for (Map.Entry<String, Map<String, String>> entry : storage.getDataStorage().entrySet()) {
+            if (value.equals(entry.getValue().get(nameParametr))) {
+
+                for (Map.Entry<String, String> entry1 : entry.getValue().entrySet()) {
+                    if (!entry1.getValue().isEmpty())
+                        System.out.println(changeText(entry1.getKey()) + ", " + entry1.getValue());
+                }
+
+                System.out.println("");
+            }
+        }
+    }
+
+
+    public void inputTasks() {
+        for (Map.Entry<String, Map<String, String>> entry : storage.getDataStorage().entrySet()) {
+
+            for (Map.Entry<String, String> entry1 : entry.getValue().entrySet()) {
+                if (!entry1.getValue().isEmpty())
+
+                    System.out.println(changeText(entry1.getKey()) + ", " + entry1.getValue());
+            }
+
+            System.out.println("");
+        }
+    }
+
+    public void addNewTask(String caption, String description, String priority, String deadline) {
+//        System.out.println("Свободный id: " + storage.getFreeId());
+        String id = storage.getFreeId();
+        Map<String, String>  element = new LinkedHashMap<>();
+        element.put("caption", caption);
+        element.put("Description", description);
+        element.put("Priority", priority);
+        element.put("Deadline", deadline);
+        element.put("Status", "new");
+        element.put("Complete", "");
+        storage.getDataStorage().put(id, element);
+
+        storage.addData(id);
+    }
+
+    public void edit(String id, String nameParametr, String data) {
+        if (storage.getListId().contains(id)) {
+            storage.getDataStorage().get(id).put(nameParametr, data);
+        } else {
+            System.out.println("Задачи с таким id не существует");
+        }
+    }
+
+    public void remove(String id) {
+        if (storage.getListId().contains(id)) {
+            storage.remove(id);
+        } else {
+            System.out.println("Задачи с таким id не существует");
+        }
+    }
+
+    public void writeDocument() {
+        storage.dataReplacement();
+    }
+
+
+    public boolean hasId(String id) {
+        if (storage.getListId().contains(id))
+            return true;
+        else
+            return false;
+    }
+
+    private String changeText(String text) {
+        if (text.equals("caption"))
+            return "Заголовок";
+        if (text.equals("Description"))
+            return "Описание";
+        if (text.equals("Priority"))
+            return "Важность";
+        if (text.equals("Deadline"))
+            return "Срок";
+        if (text.equals("Status"))
+            return "Статус";
+        if (text.equals("Complete"))
+            return "Дата когда задача была выполнена";
+        return text;
+    }
+
+
+
+
+}
